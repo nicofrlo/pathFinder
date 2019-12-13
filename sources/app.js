ad1a56f282d20461f42ea1fa098c6fffff69cd86
Nicolas Guinot <nicolas.guinot@epita.fr>
2019-12-14 00:44:52 +0100

    Added p5 to our project
    Added p5 folder to allowed routes
    Example sketch available on localhost:3000

--- sources/app.js
+++ sources/app.js
@@ -18,6 +18,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/p5')));
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
